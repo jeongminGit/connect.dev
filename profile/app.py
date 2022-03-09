58ded8514b5017c4a0bfd98c921635a53bbcdef9
gofl26 <gofl26@naver.com>
2022-03-10 02:20:47 +0900

    프로필메인 프로필작성 프로필수정

--- profile/app.py
+++ profile/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import requests
 app = Flask(__name__)
 
 from pymongo import MongoClient
@@ -7,21 +8,19 @@ import certifi
 ca = certifi.where()
 
 client = MongoClient('mongodb+srv://connect_dev:ukdzr1Y72Jilh3N0@cluster0.tgb50.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',tlsCAFile=ca)
-db = client.connect_dev
+db = client.ssr
+
 
 @app.route('/')
-def home():
-    return render_template('profilemain.html')
+def profile_main():
+    return render_template('profile_main.html')
 
-# profilemain.html용
-@app.route("/connect_dev/profile", methods=["GET"])
+@app.route("/profile_main", methods=["GET"])
 def profile_upload():
     profile = list(db.users.find({}, {'_id': False}))
     return jsonify({'profiling': profile})
 
-
-# profile.html용
-@app.route("/connect_dev", methods=["POST"])
+@app.route('/profile_mk_post', methods=["POST"])
 def profile_post():
     url1_receive = request.form['url1_give']
     url2_receive = request.form['url2_give']
@@ -32,10 +31,10 @@ def profile_post():
     num_receive = request.form['num_give']
 
     doc = {
-        'name':name_receive,
-        'job':job_receive,
-        'comment':comment_receive,
-        'num': int(num_receive),
+        'name': name_receive,
+        'job': job_receive,
+        'comment': comment_receive,
+        'num': num_receive,
         'url1': url1_receive,
         'url2': url2_receive,
         'url3': url3_receive,
@@ -44,6 +43,43 @@ def profile_post():
 
     return jsonify({'msg':'저장완료!'})
 
+@app.route('/profile_mk')
+def profile_mk():
+    return render_template('profile_mk.html')
+
+@app.route('/profile_revise')
+def profile_revise():
+    return render_template('profile_revise.html')
+
+@app.route('/profile_revise_upload', methods=["POST"])
+def profile_revise_upload():
+    url1_receive = request.form['url1_give']
+    url2_receive = request.form['url2_give']
+    url3_receive = request.form['url3_give']
+    name_receive = request.form['name_give']
+    job_receive = request.form['job_give']
+    comment_receive = request.form['comment_give']
+    num_receive = request.form['num_give']
+
+    doc = {
+        'name': name_receive,
+        'job': job_receive,
+        'comment': comment_receive,
+        'num': num_receive,
+        'url1': url1_receive,
+        'url2': url2_receive,
+        'url3': url3_receive,
+    }
+    db.users.insert_one(doc)
+
+    return jsonify()
+
+@app.route('/profile_revise_delete', methods=["POST"])
+def profile_delete():
+    name_receive = request.form['name_give']
+    db.users.delete_one({'name':name_receive})
+
+    return jsonify({'msg':'저장완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
